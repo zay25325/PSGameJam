e9bab4625bf6116ec6fe75efbfdab2f19fa44466
Andrew3369 <andrewjanosbabos@gmail.com>
2025-01-29 01:12:30 -0500

    Contract Update
    - Swapped from normal scene prefab of the contract ui onto the UI Toolkit instead
    - Seems like it reads from the file and returns null. the script works but needs fixing

--- Assets/Scripts/Contracts.cs
+++ Assets/Scripts/Contracts.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
+
+[System.Serializable]
 public class Contract
 { // individual contract
     public string title;
     public string description;
-    public string experience;
+    public int experience;
     public string difficulty;
 }
 
@@ -19,34 +22,44 @@ public class ContractList
 
 public class Contracts : MonoBehaviour
 {
+    [SerializeField] private TextAsset contractJSONFile;
+    [SerializeField] private UIDocument uiDocument;
 
+    private VisualElement root;
+    private VisualElement contractListContainer;
+    private ContractList allContracts;  // Changed from Contract to ContractList
 
-
-    private Contract allContracts;
-    private List<Contract> contractList = new();
-
-
-    // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
+        root = uiDocument.rootVisualElement;
+        if (root == null)
+        {
+            Debug.Log("UI document root is null");
+            return; 
+        }
+
+        contractListContainer = root.Q<VisualElement>("contract-list");
         LoadContracts();
+        DisplayRandomContracts();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    // load and finalize the contracts before displaying
+    //void Update()
+    //{
+
+    //}
+
     private void LoadContracts()
-    {
-
+    { // load all contracts from the file
+        allContracts = JsonUtility.FromJson<ContractList>(contractJSONFile.text);
     }
 
-    // display the contracts after loading
-    private void SetupContractUI()
+    private void DisplayRandomContracts()
     {
-
+        int randomIndex = Random.Range(0, allContracts.contracts.Length);
+        Contract randomContract = allContracts.contracts[randomIndex];
+        Debug.Log(randomContract.title);
     }
+
+
 }
